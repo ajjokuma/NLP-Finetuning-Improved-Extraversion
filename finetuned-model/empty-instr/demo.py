d07d40b2cd5ecf77b1a7025cf0e6797e9e7d02f0
Ajwa Shahid <76915659+ajjokuma@users.noreply.github.com>
2024-12-16 15:09:25 -0500

    empty instruction finetuned model

---
+++ finetuned-model/empty-instr/demo.py
@@ -0,0 +1,52 @@
+import fire
+
+from lora import Llama_Lora, Gemma_Lora, Pythia_Lora
+from huggingface_hub import login
+login(token='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')
+
+def main(
+        task: str = "train",
+        llm: str = "llama",
+        base_model: str = "meta-llama/Llama-2-7b-chat-hf",
+):
+    if len(base_model) == 0:
+        raise ValueError("Please specify the base model.")
+    if llm == "llama":
+        m = Llama_Lora(
+            base_model = base_model,
+        )
+    elif llm == "gemma":
+        m = Gemma_Lora(
+            base_model = base_model,
+        )
+    elif llm == "pythia":
+        m = Pythia_Lora(
+            base_model = base_model,
+        )
+    else:
+        raise ValueError(f"Unrecognized llm name: {llm}")
+    if task == "train":
+        m.train(
+            train_file = "data/sst2/empty_string_dataset.json",
+            # val_file = "data/sst2/val.json",
+            output_dir = f"./ckp_sst_{llm}_lora",
+            train_batch_size = 32,
+            num_epochs = 1,
+            group_by_length = False,
+            logging_steps = 5,
+            val_steps = 20,
+            val_batch_size = 8,
+        )
+    elif task == "eval":
+        m.predict(
+            input_file = "data/sst2/human-eval-generic.json",
+            lora_adapter = "./ckp_sst_llama_lora",
+            max_new_tokens = 256,
+            verbose = True,
+        )
+    else:
+        raise ValueError(f"Unrecognized task name: {task}")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ finetuned-model/empty-instr/lora/__init__.py
@@ -0,0 +1,9 @@
+# 
+from .llama_lora import Llama_Lora
+from .falcon_lora import Falcon_Lora
+from .flan_t5_lora import Flan_T5_Lora
+from .mistral_lora import Mistral_Lora
+from .gemma_lora import Gemma_Lora
+from .pythia_lora import Pythia_Lora
+
+
